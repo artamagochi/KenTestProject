30eb36a0c476edda560210e6eb9e536c79084e75
kramirez <ken.rp8@gmail.com>
2020-10-17 23:43:50 -0500

    first project commit

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import styles from './assets/styles/styles.js';
+import Header from './assets/components/header';
+import TodoItem from './assets/components/todoItem';
+
+
+export default function App() {
+
+  const [todos, setTodos] = useState([
+    {text: 'fisrt', key: '1'},
+    {text: 'second', key: '2'},
+    {text: 'thrid', key: '3'},
+  ])
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) =>{
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
+
+  return (
+    <View style = {styles.container}>
+      <Header />
+      <View style={styles.content}>
+        {/*to form*/}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({item}) => (
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
---
+++ assets/styles/styles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eaeaea",
+  },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
+  header: {
+    height: 80,
+    paddingTop: 38,
+    backgroundColor: 'coral',
+  },
+});
